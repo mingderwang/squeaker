a6ff938c90d06fc7b4e356acb505a281d5709639
Ming-der Wang <mingder78@yahoo.com>
2015-04-01 22:53:24 +0800

    init generated by rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squeaker::Application.config.secret_token = '5df776b813f49f5bf120bae5b621130622b2e0d582711b88c5fcd2e1b82d13be63155fb87b8ed81126f9c284301cfea66c6b62a4012b9188c306ee1f21d8cc3a'
