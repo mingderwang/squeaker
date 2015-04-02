194a9daab9ea9b1437b7d2ceed35085b90a2c57a
Ming-der Wang <mingder78@yahoo.com>
2015-04-02 22:46:48 +0800

    add Message mode and associate to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150402123651) do
+ActiveRecord::Schema.define(:version => 20150402144350) do
+
+  create_table "messages", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
--- features/support/factories.rb
+++ features/support/factories.rb
@@ -4,4 +4,8 @@ FactoryGirl.define do
 	factory :user do |f|
 	f.username 'testuser'
 	end
+	factory :message do |f|
+		f.association :user
+		f.content 'Test message content'
+	end
 end
